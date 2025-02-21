4922f911daf81440d3863c6db69f6c351115210b
angelotavio <139417327+angelotavio@users.noreply.github.com>
2025-02-21 17:00:28 -0300

    delet

--- README.md
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:fc67ba316548cf2da08e34ffea11d3c6efe8264fbca9fe4226dabc8ebfc8698c
-size 46
--- app.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9a13d92cbe279bfafebf53e89215d85d6644fea00edbe13ce507e2564e9efc30
-size 1950
